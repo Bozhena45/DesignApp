3e93545682274a6dd4ec30e566dfac24df3aeaad
usuario <usuario@linux-axk4.edib.es>
2020-02-27 10:29:41 +0100

    Comment the class comments

--- comments.h
+++ comments.h
@@ -3,15 +3,42 @@
 #include "json.hpp"
 
 using json = nlohmann::json;
-
+/**
+ * @brief La clase comentarios
+ *
+ * Con esta clase podremos crear un comentario y coger el id puesto que es privado(lo cogeríamos a partir de un método)
+ * Además  se podrá generar/crear un JSON a partir de la libreria json = nlohmann::json
+ */
 
 class Comments
 {
 public:
+
+    /**
+     * @brief Los constructores de la clase comentario
+     */
     Comments();
     Comments(std::string comment,int idPhoto,int idUser);
+
+    /**
+     * @brief CreateComment
+     *
+     * A partir de este método crearíamos nuestro comentario
+     */
     void CreateComment();
+
+    /**
+     * @brief toJSON
+     * @return Este método devuelve los mensajes en forma de json
+     */
     json toJSON();
+    /**
+     * @brief setId
+     *
+     *  Sirve para coger el ID que es un parámetro privado
+     * @param id
+     */
+
     void setId(int id);
 
 private:
